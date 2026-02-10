a35ed83db148183aa6d8e0752bf586a304c26a48
gmp <97922012+gpaesano@users.noreply.github.com>
2026-02-10 14:30:19 +0100

    Update kernel branch for Jammy configuration

--- config/suites/jammy.sh
+++ config/suites/jammy.sh
@@ -4,7 +4,8 @@ export RELASE_NAME="Ubuntu 22.04 LTS (Jammy Jellyfish)"
 export RELASE_VERSION="22.04"
 
 export KERNEL_REPO="https://github.com/hqnicolas/linux-rockchip.git"
-export KERNEL_BRANCH="jammy-debug"
+# export KERNEL_BRANCH="jammy-debug"
+export KERNEL_BRANCH="rk-6.1-rk5"
 export KERNEL_FLAVOR="rockchip"
 
 export EXTRA_PPAS="jjriek/rockchip jjriek/rockchip-multimedia"
